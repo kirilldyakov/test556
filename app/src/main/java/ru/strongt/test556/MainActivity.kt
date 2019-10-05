40d90b549fe1c551e430ed60afb568be68b84d8d
Kirill Dyakov <Dyakovks@gmail.com>
2019-10-05 11:19:34 +0300

    tst-02 Добавлена обработка нажатия

--- app/src/main/java/ru/strongt/test556/MainActivity.kt
+++ app/src/main/java/ru/strongt/test556/MainActivity.kt
@@ -2,11 +2,15 @@ package ru.strongt.test556
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        main_activity_button_push_me.setOnClickListener { Toast.makeText(this, "Bingo!!", Toast.LENGTH_LONG).show() }
     }
 }
